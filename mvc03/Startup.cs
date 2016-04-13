bae21344cc4714f6e4087831ffcd3ce1a9be2672
maheavenlu <luqingda1994@163.com>
2016-04-13 14:30:36 +0800

    添加项目文件。

---
+++ mvc03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc03.Startup))]
+namespace mvc03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
